1ee76a12ef75b2d2805c050992cdfd8dd3983869
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2025-01-23 19:50:36 -0500

    Add cursor

--- webserver.py
+++ webserver.py
@@ -20,8 +20,8 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False,
     max_num_hands=2,
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.5
+    min_detection_confidence=0.75,
+    min_tracking_confidence=0.75
 )
 
 # Store hand symbols
